e730c556bcdad92cea57fc223fb5a5f8acd2fca3
georgewrmarshall <george.marshall@consensys.net>
2022-10-04 15:04:29 -0700

    Removing console.log

--- ui/components/component-library/icon/icon.stories.js
+++ ui/components/component-library/icon/icon.stories.js
@@ -93,7 +93,6 @@ export const DefaultStory = (args) => <Icon {...args} />;
 DefaultStory.storyName = 'Default';
 
 export const Name = (args) => {
-  console.log('ICON_NAMES', ICON_NAMES);
   const [search, setSearch] = useState('');
   const iconList = Object.keys(ICON_NAMES)
     .filter(
